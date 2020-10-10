c3484d5ed88bb44cc3eb391ee70055acc1aa23f3
AB1902 <62405016+AB1902@users.noreply.github.com>
2020-10-10 21:49:24 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import Location from './Location'
-import './App.css';
+import React from 'react'
+import ReactTable from "react-table-6";  
+import "react-table-6/react-table.css" 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Location></Location>
-      </header>
-    </div>
-  );
+// import {contact,notif,hospital} from './components'
+// import {fetchdata} from './api'
+import {fetchdata4} from './api'    
+
+class App extends React.Component  {
+
+    constructor(props){
+        super(props);
+        
+        this.state={
+            posts:[]
+        }
+    }
+
+    async componentDidMount(){
+        const data= await fetchdata4()
+
+         var posts=data
+        //  this.setState({
+        //      posts:posts
+        //  })
+
+        console.log(posts)
+    }
+
+    render(){
+        // const columns=[
+        //     {
+        //         Header:"location",
+        //         accessor:"loc"
+        //     },
+        //     {
+        //         Header:"number",
+        //         accessor:"number"
+        //     }
+        // ]
+        return(
+            <div>
+                <h1 >HELPLINE NUMBERS</h1>
+                {/* <ReactTable columns={columns} data={this.state.posts}></ReactTable> */}
+            </div>
+        )
+    }
 }
 
-export default App;
+ export default App
